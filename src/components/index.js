0820c45bbcf5ddd0981839f21080d17cd8a2c7ea
akash <akashkamble8830@gmail.com>
2022-10-20 21:30:36 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Bussiness from "./Bussiness";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Testimonial from "./Testimonial";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Bussiness,
+    CTA,
+    Clients,
+    Stats,
+    Footer,
+    Hero,
+    Testimonial
+};
